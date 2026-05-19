0163a7eb75aec433ddc3301082f3d6a08ad5f53e
kim minjeong <minjeong7789@naver.com>
2026-05-19 12:49:58 +0900

    feat: 후기 작성 가능 상태 응답 추가

--- src/main/java/com/nbang/GongguMinjok/repository/MannerReviewRepository.java
+++ src/main/java/com/nbang/GongguMinjok/repository/MannerReviewRepository.java
@@ -9,6 +9,8 @@ public interface MannerReviewRepository extends JpaRepository<MannerReview, Long
 
     boolean existsByGroupBuyIdAndReviewerId(Long groupBuyId, Long reviewerId);
 
+    List<MannerReview> findByReviewerId(Long reviewerId);
+
     List<MannerReview> findByRevieweeId(Long revieweeId);
 
     List<MannerReview> findByGroupBuyId(Long groupBuyId);
