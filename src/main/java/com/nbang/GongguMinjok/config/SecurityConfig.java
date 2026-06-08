175176bb21a862b43e797aa2f3f3f47b7ee61d5b
kim minjeong <minjeong7789@naver.com>
2026-06-08 20:33:30 +0900

    fix: PATCH 추가

--- src/main/java/com/nbang/GongguMinjok/config/SecurityConfig.java
+++ src/main/java/com/nbang/GongguMinjok/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
                 "https://gongguminjok.com",
                 "https://www.gongguminjok.com"
         ));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
